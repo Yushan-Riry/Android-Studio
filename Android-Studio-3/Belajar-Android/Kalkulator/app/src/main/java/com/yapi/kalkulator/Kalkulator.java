e5476752096f4ccbc0595ec1816d39c390a7e470
Yushan Aisyah P.I <68349067+Yushan-Riry@users.noreply.github.com>
2021-07-30 13:42:37 +0700

    Minggu Ke-3
    Android Studio

---
+++ Android-Studio-3/Belajar-Android/Kalkulator/app/src/main/java/com/yapi/kalkulator/Kalkulator.java
@@ -0,0 +1,13 @@
+package com.yapi.kalkulator;
+
+public class Kalkulator {
+
+    public double penjumlahan (double bil_1, double bil_2) { return bil_1 + bil_2; }
+
+    public double pengurangan (double bil_1, double bil_2) { return bil_1 - bil_2; }
+
+    public double perkalian (double bil_1, double bil_2) { return bil_1 * bil_2; }
+
+    public double pembagian (double bil_1, double bil_2) { return bil_1 / bil_2; }
+
+}
