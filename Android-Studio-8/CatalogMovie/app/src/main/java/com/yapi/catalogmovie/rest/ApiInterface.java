e5ba29e56bce1cf488c0b5d1ccb4211015c48ad1
Yushan Aisyah P.I <68349067+Yushan-Riry@users.noreply.github.com>
2021-09-03 23:45:38 +0700

    Minggu Ke-8
    Android Studio

---
+++ Android-Studio-8/CatalogMovie/app/src/main/java/com/yapi/catalogmovie/rest/ApiInterface.java
@@ -0,0 +1,26 @@
+package com.yapi.catalogmovie.rest;
+
+import com.yapi.catalogmovie.model.Response;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("/3/movie/{category}")
+    Call<Response> getMovie(
+            @Path("category") String category,
+            @Query("api_key") String api_key,
+            @Query("language") String languange,
+            @Query("age") int page
+    );
+
+    @GET("/3/search/movie")
+    Call<Response> getQuery(
+            @Query("api_key") String api_key,
+            @Query("language") String language,
+            @Query("query") String query,
+            @Query("page") int page
+    );
+}
